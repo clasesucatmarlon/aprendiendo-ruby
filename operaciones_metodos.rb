abfba67038a63549ea1217483370b0664b4513f1
Marlon García <clasesucatmarlon@gmail.com>
2021-09-06 11:53:15 -0500

    operaciones de suma y resta

--- operaciones_metodos.rb
+++ operaciones_metodos.rb
@@ -1,6 +1,12 @@
-def operaciones(var1, var2)
+def suma(var1, var2)
   resultado = var1 + var2
   puts resultado
 end
 
-operaciones(20, 30)
+def resta(var1, var2)
+  resultado = var1 - var2
+  puts resultado
+end
+
+suma(20, 30)
+resta(20, 30)
