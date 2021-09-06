ca51a789c96451578090eaaf68fe1516ec473f79
Marlon García <clasesucatmarlon@gmail.com>
2021-09-06 18:27:59 -0500

    Caprurar datos - función chomp

--- capturar_datos.rb
+++ capturar_datos.rb
@@ -1,3 +1,10 @@
+def saludo(nombre, mensaje)
+  puts "Hola #{nombre}, #{mensaje}"
+  
+end
+
 print "Cuál es tu nombre: "
 nombre = gets()
-puts "Hola #{nombre}, cómo estás?"
+# obliga a que la salida sea en la misma línea
+nombre = nombre.chomp 
+saludo(nombre, "Cómo estás...")
