4b839003ac07c0de0818c7faaa371f7bb3da1313
Marlon García <clasesucatmarlon@gmail.com>
2021-09-06 10:36:10 -0500

    Variables

---
+++ variables.rb
@@ -0,0 +1,12 @@
+# TRABAJANDO CON VARIABLES
+numero1 = 10.0
+numero2 = 20
+puts numero1
+
+nombre = 'Marlon'
+puts 'Hola ' + nombre
+nombre = 'Marlon García'
+puts 'Hola ' + nombre
+
+resultado = numero1 / numero2
+puts 'resultado de la suma es: ', resultado
