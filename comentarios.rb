fa01567f81e5cdc1f79a80b2723bdc4ca29a2d46
Marlon García <clasesucatmarlon@gmail.com>
2021-09-06 09:56:44 -0500

    Comentarios multilínea

--- comentarios.rb
+++ comentarios.rb
@@ -1,3 +1,11 @@
-# COMENTARIOS EN RUBY
-puts 'Hol Marlon'
+# COMENTARIOS DE UNA LINEA
+puts 'Hola Marlon !!!!!'
 puts 'bienvenido'
+
+# COMENTARIOS MULTILINEA
+=begin
+Esto es un comentario 
+multilínea
+=end
+puts 'después de comentario multilinea'
+
