414177f300fc1b474a38fa1a4c75a5c66eef51f6
Marlon García <clasesucatmarlon@gmail.com>
2021-09-06 18:11:28 -0500

    Obtener tipo de datos

---
+++ tipos_datos.rb
@@ -0,0 +1,9 @@
+nombre = "Marlon"
+edad = 48
+promedio = 12.3
+status = false
+
+puts "la variable nombre es de tipo: #{nombre.class}"
+puts "la variable edad es de tipo: #{edad.class}"
+puts "la variable promedio es de tipo: #{promedio.class}"
+puts "la variable status es de tipo: #{status.class}"
